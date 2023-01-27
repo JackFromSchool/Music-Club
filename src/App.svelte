c8e5400f468333f1f820363f24f24974d9c81e8e
BuildTools <unconfigured@null.spigotmc.org>
2023-01-26 23:45:20 -0600

    Init Commit

---
+++ src/App.svelte
@@ -0,0 +1,58 @@
+<script>
+</script>
+
+<main>
+    <header>
+      <p class="header-text">BHSMC</p>
+    </header>
+    <div class="main-container">
+      <p class="statement">Big Things Are Coming</p>
+      <p class="the-what">A place to share and discover new music<br>Learn about different genres<br>And more...</p>
+    </div>
+</main>
+
+<style>
+  main {
+    text-align: center;
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    gap: 1rem;
+    min-height: 3rem;
+  }
+
+  p {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+
+  .main-container {
+    display: flex;
+    min-height: calc(100vh - 3rem);
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .statement {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: bold;
+    font-size: 2rem;
+    line-height: 2.5rem;
+  }
+
+  .the-what {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  .header-text {
+    font-family: 'Unbounded', cursive;
+    font-size: 2rem;
+    margin: 0;
+    padding-top: 1rem;
+    padding-left: 3vw;
+  }
+
+</style>
